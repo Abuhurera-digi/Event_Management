aa44eedc4afd7e53c9ebb2d9d5c6ae363cdf8b1e
Abuhurera579 <ahurera@digisprint.com>
2022-11-07 10:40:54 +0530

    commit 10

--- src/main/java/com/digisprint/Event_Management1/Contoller/birthdayController.java
+++ src/main/java/com/digisprint/Event_Management1/Contoller/birthdayController.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
